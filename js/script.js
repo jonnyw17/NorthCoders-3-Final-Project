0fb70e2cb257aeb62fba68f56ae080add998519c
jonnyw17 <jonny@spaceportx.com>
2017-08-03 14:50:26 +0100

    zoom-out fixed, need to now fix zoom-in

--- js/script.js
+++ js/script.js
@@ -41,32 +41,84 @@ setupPlanet('outer-spacee', 300, 0.01607 );
 
 $(document).ready(function(){
 
-var counter = 0;
+  //ZOOM STATE COUNTER 1/2/3
+  var counter = 0;
 
+
+
+  // BUTTON BAR ACTIVE CLASS ENGAGED AT COUNTER 0
+  if(counter === 0){
+    $('#solar-system3').addClass('btn-active');
+    // $('.galaxy').addClass('active');
+  }
+
+  counterTest = function (){
+   if (counter === 1){
+     //navbar button state changed if condition is met
+    $('#solar-system3').removeClass('btn-active');
+    $('#solar-system2').addClass('btn-active');
+    //if zoom-out condition is met solar system 'zooms-out'
+    $('#astroid').addClass('zoom-out');
+    $('.galaxy').addClass('active');
+    //planet info button fades out due to none relevance
+    $('#planet-info').fadeOut(1000, function(){
+    $('#planet-info').css({'display' : 'none'})
+    });
+
+
+
+    //if zoom-in is triggered solar system reappears/galaxy fade out setting back to state 1(solar system)
+    // $('#zoom-in').on('click', function(){
+    //   $('#astroid').addClass('zoom-in');
+    //   $('.galaxy').removeClass('active')
+    //   $('#solar-system3').removeClass('btn-active');
+    //   $('#solar-system2').addClass('btn-active');
+    // });
+
+  }else if(counter === 2){
+    //navbar button state changed if condition is met (galaxy - outerspace)
+      $('#solar-system2').removeClass('btn-active');
+      $('#solar-system1').addClass('btn-active');
+      //galaxy 'zooms out' outerspace galaxies appear
+      $('.galaxy').removeClass('active');
+      $('.outer-space').addClass('active');
+      $('#zoom-in').on('click', function(){
+        $('.galaxy').addClass('zoom-in');
+        $('.outer-space').removeClass('active')
+        $('#solar-system3').removeClass('btn-active');
+        $('#solar-system2').addClass('btn-active');
+      });
+
+  // }else if(counter === 3){
+  //     //galaxy 'zooms out' outerspace galaxies appear
+  //     // $('.galaxy').removeClass('active');
+  //     // $('.outer-space').addClass('active');
+  //
+   }
+}
+
+  // PLANET INFO TABS FADE IN TOGGLE SET FOR COUNTER 0
   $('#planet-info').click(function(){
     $('#wiki-nav-click1, #wiki-nav-click2').fadeToggle(500)
   });
 
-  $('#zoom-out').on('click', function(){
-    counter++;
-    $('#astroid').addClass('zoom-out');
-    $('.galaxy').addClass('active');
-    $('#planet-info').fadeOut(1000, function(){
-      $('#planet-info').css({'display' : 'none'})
-    })
-    if(counter === 1){
-        $('#zoom-in').on('click', function(){
-          $('#astroid').addClass('zoom-in');
-          $('.galaxy').removeClass('active')
-      })
+  // SET ZOOM STATES (COUNTER 0 = SOLAR SYSTEM, COUNTER 1 = GALAXY, COUNTER 3 = OUTERSPACE)
+  $('#zoom-out').on('click', function() {
 
-    }else if(counter === 2){
-      console.log(counter)
-      $('#zoom-out').on('click', function(){
-        $('.galaxy').removeClass('active');
-        $('.outer-space').addClass('active');
-      });
-    }
-    console.log(counter)
-});
+
+    if (counter >= 2) {
+
+      return
+    };
+
+    counter+=1;
+    counterTest();
+
+  });
+
+  $('#zoom-in').on('click', function(){
+    counter-=1;
+    counterTest();
+
+  });
 });
